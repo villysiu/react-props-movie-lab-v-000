a953cad3edec1cc48b16f338e107f43ac101c1db
villysiu <villysiu@gmail.com>
2021-07-12 13:10:31 -0700

    commit tree 2026e84d6b01c03cefd301a36a24df8f99c6cace

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -30,7 +30,10 @@ const posterMap = {
 export default class MovieCard extends Component {
 
   render() {
+    console.log(this.props.data)
+  
     return (
+      
       <div className="movie-card">
         <CardFront poster={posterMap[this.props.poster]} />
         <CardBack
